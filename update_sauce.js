40570bca8fcaf671c79ba5e567ab12a9c0fecb83
Brandon Myers <trakkasure@gmail.com>
2015-08-20 00:32:06 -0500

    Sauce update?

--- update_sauce.js
+++
@@ -1,6 +0,0 @@
-
-
-console.log('*** Completed test ****');
-console.log('***********************');
-console.log('Environment:');
-console.log(JSON.stringify(process.env));
--- wct.conf.js
+++ wct.conf.js
@@ -1,3 +1,4 @@
+var request = require('request');
 
 module.exports = {
     verbose: false,
@@ -14,5 +15,34 @@ module.exports = {
             ],
             'tunnelId': process.env.TRAVIS_JOB_NUMBER
         }
+    },
+    registerHooks: function(wct) {
+         wct.on('browser-end', function(def, error, stats, sessionId, browser) {
+            if (eachCapabilities.length === 0 || !sessionId) return;
+
+            if (browser._keepalive) {
+              clearInterval(browser._keepalive);
+            }
+
+            var payload = {
+              "name": process.env.TRAVIS_BRANCH+"_"+process.env.TRAVIS_COMMIT,
+              "build": process.env.TRAVIS_BUILD_NUMBER,
+              "public": shared
+            };
+            if (process.env.TRAVIS_TAG)
+                payload.tag = process.env.TRAVIS_TAG;
+
+            wct.emit('log:debug', 'Updating sauce job', sessionId, payload);
+
+            // Send the pass/fail info to sauce-labs if we are testing remotely.
+            var username  = wct.options.plugins.sauce.username;
+            var accessKey = wct.options.plugins.sauce.accessKey;
+            request.put({
+              url:  'https://saucelabs.com/rest/v1/' + encodeURIComponent(username) + '/jobs/' + encodeURIComponent(sessionId),
+              auth: {user: username, pass: accessKey},
+              json: true,
+              body: payload,
+            });
+          });
     }
 };
